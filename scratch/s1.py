fc4494a552ddcda4809ecbcf94e7e52abc5d7298
boyerb <bhb35@byu.edu>
2024-09-20 11:08:31 -0600

    Updates

--- scratch/s1.py
+++ scratch/s1.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join('..')))
-from functions import bbf_functions as invf
+from functions import bates_boyer_fletcher_functions as invf
 import pandas as pd
 
 tickers = ['AAPL', 'MSFT', 'GOOGL', 'VOO']

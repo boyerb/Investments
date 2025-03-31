196cfe95b6e377d6ff86af789abd820ad36c2b72
boyerb <bhb35@byu.edu>
2025-03-31 10:57:23 -0600

    Updates

--- functions/simple_finance.py
+++ functions/simple_finance.py
@@ -10,6 +10,8 @@ import urllib.request
 import statsmodels.api as sm
 import numpy as np
 from scipy.optimize import minimize
+from scipy.stats import norm
+from scipy.optimize import brentq
 
 pd.set_option('display.max_rows', None)  # Show all rows
 pd.set_option('display.max_columns', None)  # Show all columns
@@ -570,3 +572,27 @@ def tangent_portfolio(expected_returns, covariance_matrix, rf=None, factors=None
 
     return tangent_weights, tangent_return, tangent_volatility
 ###################################################################################################
+def black_scholes(type, S, K, T, rf, sigma):
+
+    r=np.log(1+rf) # create continuous-time risk-free rate
+
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    if type == "call":
+        option_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    elif type == "put":
+        option_price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+    else:
+        raise ValueError("Invalid option type. Choose 'call' or 'put'.")
+
+    return option_price
+##################################################################################################
+def implied_volatility(type, option_price, S, K, T, r):
+    def objective(sigma):
+        return black_scholes(S, K, T, r, sigma, option_type) - option_price
+
+    try:
+        return brentq(objective, 1e-6, 5.0)
+    except ValueError:
+        return np.nan
